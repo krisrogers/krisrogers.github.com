3d011263ada8783521d3b7425bcb629770fcc7d6
Kris Rogers <kris@kapiche.com>
2015-06-19 21:56:06 +1000

    Finished first implementation of timeline.
    Resume tweaks for consistency.

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -4,6 +4,12 @@
     draw(data["HISTORY"], data["TOTAL_YEARS"], data["COLOURS"]);
 }());
 
+Opentip.styles.history = {
+    extends: "dark",
+    width: 400
+};
+Opentip.defaultStyle = "history";
+
 
 // Main draw routine
 function draw(HISTORY, TOTAL_YEARS, COLOURS) {
@@ -19,7 +25,6 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
         axis,
         axisLabels,
         backgroundSections,
-        outerArcs,
         middleArcs,
         innerArcs;
 
@@ -42,7 +47,7 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
                 h.x = x;
                 x += h.segmentWidth;
             } else {
-                h.colour = COLOURS[i++];
+                h.colour = COLOURS[i];
                 h.segmentWidth = h._dummy ? dummySegmentWidth : Math.floor(h.years / TOTAL_YEARS * availableWidth);
                 h.x = x;
                 x += h.segmentWidth;
@@ -51,12 +56,19 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
                 var r0 = h.segmentWidth * 0.4,
                     r1 = r0 * 0.5,
                     r2 = r1 * 0.7,
-                    r3 = 0;
+                    r3 = 0,
+                    startAngle = 260 * (Math.PI / 180);
                 h.outerArcGenerator = d3.svg.arc().innerRadius(r1).outerRadius(r0);
                 h.middleArcGenerator = d3.svg.arc().innerRadius(r2).outerRadius(r1);
                 h.innerArcGenerator = d3.svg.arc().innerRadius(r3).outerRadius(r2);
-                h.startAngle = 270 * (Math.PI / 180);
-                h.endAngle = h.startAngle;  // real end angle will be set via tweening
+                h.startAngle = startAngle;
+                h.endAngle = h.startAngle + 200 * Math.PI / 180;
+                h.facets.forEach(function (f) {
+                    f.startAngle = startAngle;
+                    f.endAngle = f.startAngle + 200 * f.value * Math.PI / 180;
+                    startAngle = f.endAngle;
+                });
+                i++;
             }
         });
     }());
@@ -104,38 +116,49 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
                     .attr("class", "axis-label-background")
                     .attr("x", bb.x - 2)
                     .attr("width", bb.width + 5)
-                    .attr("y", bb.y - 2)
-                    .attr("height", bb.height + 5)
+                    .attr("y", bb.y)
+                    .attr("height", bb.height + 2)
                     .attr("rx", 2)
                     .attr("ry", 2)
             })
             .moveToFront();
-        // Spacer beneath axis
-        /*svg.append("rect")
-            .attr("class", "axis-spacer")
-            .attr("x", 0)
-            .attr("width", width)
-            .attr("y", y - segmentTickHeight)
-            .attr("height", segmentTickHeight);*/
     }());
 
     (function drawArcs () {
         var y = axisY - segmentTickHeight,
             data = HISTORY.filter(function (h) {
                 return !h._dummy;
+            }),
+            opacityScale = d3.scale.pow().domain([0, 1]).range([0.4, 0.8]);
+
+        data.forEach(function (d) {
+            var cumulativeValue = 0;
+            d.facets.forEach(function (f) {
+                cumulativeValue += f.value;
+                // Facet chunk
+                var facetChunk = svg.append("path")
+                    .attr("class", "arc-outer")
+                    .attr("data-ot", f.description)
+                    .attr("data-ot-title", f.name)
+                    .attr("d", function () {
+                        return d.outerArcGenerator(f);
+                    })
+                    .attr("transform", function () {
+                        return "translate(" + (d.x + d.segmentWidth / 2) + "," + y + ")";
+                    })
+                    .style("fill", function () {
+                       var col = d3.rgb(d.colour);
+                       f.colour = "rgba(" + col.r + "," + col.g + "," + col.b + "," + opacityScale(cumulativeValue) + ")";
+                       return f.colour;
+                    })
+                    .on("mouseover", function () {
+                        d3.select(this).style("fill", d.colour);
+                    })
+                    .on("mouseout", function () {
+                        d3.select(this).style("fill", f.colour);
+                    });
             });
-        outerArcs = svg.selectAll("arc-outer").data(data).enter().append("path")
-            .attr("class", "arc-outer")
-            .attr("d", function (d) {
-                return d.outerArcGenerator(d);
-            })
-            .attr("transform", function (d) {
-                return "translate(" + (d.x + d.segmentWidth / 2) + "," + y + ")";
-            })
-            .style("fill", function (d, i) { 
-                return d.colour;
-            })
-            .transition().duration(750).call(arcTween, 450 * (Math.PI / 180), "outerArcGenerator");
+        });
         middleArcs = svg.selectAll("arc-middle").data(data).enter().append("path")
             .attr("class", "arc-middle")
             .attr("d", function (d) {
@@ -143,8 +166,7 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
             })
             .attr("transform", function (d) {
                 return "translate(" + (d.x + d.segmentWidth / 2) + "," + y + ")";
-            })
-            .transition().duration(750).call(arcTween, 450 * (Math.PI / 180), "middleArcGenerator");
+            });
         innerArcs = svg.selectAll("arc-inner").data(data).enter().append("path")
             .attr("class", "arc-inner")
             .attr("d", function (d) {
@@ -155,8 +177,7 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
             })
             .style("fill", function (d, i) { 
                 return d.colour;
-            })
-            .transition().duration(750).call(arcTween, 450 * (Math.PI / 180), "innerArcGenerator");
+            });
         // Arc ticks
         svg.selectAll(".arc-tick").data(data).enter().append("path")
             .attr("class", "arc-tick")
@@ -164,17 +185,32 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
             .attr("transform", function (d) {
                 return "translate(" + (d.x + d.segmentWidth / 2) + "," + (y + 3) + ")";
             })
-            .transition().duration(750)
             .style("fill", function (d) {
                 return d.colour;
             });
+        // Tick lines
+        svg.selectAll(".arc-tick").each(function (d) {
+            var x = d.x + d.segmentWidth / 2;
+            svg.append("path")
+                .attr("class", "arc-tick-line")
+                .attr("d", lineGenerator([
+                    [x, y + 10],
+                    [x, y + 60]
+                ]))
+                .style("stroke", d.colour);
+            svg.append("circle")
+                .attr("class", "arc-tick-line-end")
+                .attr("cx", x)
+                .attr("cy", y + 65)
+                .attr("r", 5)
+                .style("stroke", d.colour);
+        });
     }());
 
     (function drawHistoryDescriptions () {
         var data = HISTORY.filter(function (h) {
             return !h._dummy;
-        }),
-        marginLeft = container.node().offsetLeft;
+        });
         container.selectAll(".history-description").data(data).enter().append("div")
             .attr("class", "history-description")
             .html(function (d) {
@@ -184,9 +220,9 @@ function draw(HISTORY, TOTAL_YEARS, COLOURS) {
             })
             .style({
                 "left": function (d) {
-                    return d.x + marginLeft + dummySegmentWidth;
+                    return d.x + dummySegmentWidth;
                 },
-                "top": axisY + 60,
+                "top": axisY + 80,
                 "width": function (d) {
                     return d.segmentWidth - dummySegmentWidth;
                 }
